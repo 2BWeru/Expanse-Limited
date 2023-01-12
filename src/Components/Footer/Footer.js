c6e5c10d9f4ef241664e39070357dc3c39cf759d
Wamaitha <wamaithaweru19@gmail.com>
2023-01-12 16:25:53 +0300

    edit background color, text color and hr line

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,64 +1,68 @@
 import React from "react";
+import "./Footer.css";
+
+
+import logo from "../../static/images/logo.png";
+
 
 
 function Footer () {
+    const today = new Date();
+
+    const currentYear = today.getFullYear();
+
     return(
-        <div class="footer" id="contact">
+        <div class="footer" id="contact" className="con">
          <footer>
             <div class="titlepage">
-               <h2>Contact</h2>
+               {/* <h2>Contact</h2> */}
 
             </div>
              <div class="container">
+                 <hr className="line"></hr>
                  <div class="row">
-                     <div class="col-sm-6 col-md-3 item" id="center">
-                         <ul>
-                           <li><h4>Visit us at :</h4></li>
-                             <li>Uniafric House, Koinange Street</li>
-                            
-                             <li><h4>Email :</h4></li>
-                             <li><a href="mailto:expanseproducts@gmail.com">expanseproducts@gmail.com</a></li>
-                            
-                             <li><h4>Tel :</h4></li>
-                             <li>0722 437 210 /</li>
-                             <li>0721 576 598</li>
-                             
-                         </ul>
+                    <div class="col-md-3">
+                        <img src={logo}></img>
+                        <p className="logo-text"><span className="logo-head">Expanse  </span> Products Ltd</p>
+                    </div>  
+                    <div class="col-md-3">
+                       <h3  className="Header-foot">Company</h3> 
+                       
+                           <h6><a href="#">About</a></h6>
+                           <h6><a href="#">We Work With</a></h6>
+                           <h6><a href="#" >Why Choose Us</a></h6>
+                           <h6><a href="#">Contact </a></h6>
+                         
+                    </div>
+                    <div class="col-md-3">
+                        <h3 className="Header-foot">Contact</h3>
+                        <span className="footer-header">Call  : </span>
+                        <h6>0722 437 210</h6>
+                        <h6>0721 576 598</h6>
 
-                     
-                     </div>
-                     <div class="col-sm-6 col-md-3 item" id="center">
-                         <ul>
-                           <li><h4>Postal Address :</h4></li>
-                           <li> P.O BOX, 7544-00100</li>
-                      
-                          
-                         </ul>
-                     </div>
-                     <div class="col-md-6 item text">
-                         <div class="map">
-                           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8188399700166!2d36.81547471419097!3d-1.2824996990645796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d3b9a0bfef%3A0x742b7ba0a98ad2ba!2sUniafric%20House%2C%2025%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1660501200148!5m2!1sen!2ske"  
-                            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                                
-                            </iframe>
-                           
-                         </div>
-                     </div>
-                     <div class="col item social">
-                         <a href="#"><i class="icon ion-social-facebook"></i></a>
-                         <a href="#"><i class="icon ion-social-twitter"></i></a>
-                         <a href="#"><i class="icon ion-social-snapchat"></i></a>
-                         <a href="#"><i class="icon ion-social-instagram"></i></a>
-                     </div>
+                        <span className="footer-header">Email us at : </span>
+                        <h6 className="footer-text"><a href="mailto:expanseproductsltd@gmail.com">expanseproductsltd@gmail.com</a></h6>
+                        
+                    </div>
+                    <div class="col-md-3">
+                        <h3 className="Header-foot">Location</h3>
+                        <span className="footer-header">Visit Us At : </span>
+                        <h6 className="footer-text">Uniafric House,Koinange street</h6>
+
+                        <span className="footer-header">Postal Address :  </span>
+                        <h6 className="footer-text">P.O BOX, 7544-00100</h6>
+                    </div>
+                   
                  </div>
              </div>
              
          </footer>
+         <div class="copyright">
+            <h4 class="copyright">Expanse Products Ltd © {currentYear}</h4>
+          </div>
      </div>
 
-    //  <div class="copyright">
-    //    <h4 class="copyright">Expanse Products Ltd © 2022</h4>
-    //  </div>
+    //  
     
     )
 }
