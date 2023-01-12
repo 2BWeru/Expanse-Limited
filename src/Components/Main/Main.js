e645d328f25469f40df1556bd04fbd9192b01922
Wamaitha <wamaithaweru19@gmail.com>
2023-01-12 16:23:13 +0300

    add Main text content

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,38 @@
+import React from "react";
+import main from "../../static/images/pr2.jpg"
+import "./Main.css";
+
+function Main () {
+    return(
+        <div id="vision"  class="vision">
+        <div class="container">
+           <div class="row d_flex">
+              <div class="col-md-12">
+                 <div class="titlepage">
+                 <h2>Why Choose Us ?</h2>
+              <span></span>
+              
+                 
+                 <p>
+                    Our briquettes are made entirely 
+                    from locally sourced sawdust waste and specially
+                    crafted for industrial boilers.
+                 </p>
+               
+                 <p>
+                    We care about your broilers' durability and sustainability,
+                    hence, we use natural binding agent for super strong briquettes.
+                 </p>
+                    
+                 <p>
+                    As no chemicals or additional additives are used, our sawdust briquettes ensure that your broilers run efficiently.
+                 </p>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </div>
+    )
+}
+
+export default Main;
